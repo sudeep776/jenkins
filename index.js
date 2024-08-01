efcbafdb0316301822204b6acbfe2d4e41345288
Sudeep U <Sudeep7766@gmail.com>
2024-08-01 21:56:02 +0530

    nodejsapp1

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0' ,() => {
   console.log(`App listening at http://localhost:${port}`);
 });
